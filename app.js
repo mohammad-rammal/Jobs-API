c0d6843f139a3382416bd72cfeb8ab7ad52aa22c
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-09-15 23:53:29 +0300

    Swagger

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const cors = require('cors');
 const rateLimiter = require('express-rate-limit');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const connectDB = require('./db/connect');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const app = express();
 
@@ -29,6 +33,11 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
+
+app.get('/', (req, res) => {
+    res.send('<h1> Jobs API </h1> <a href="/api-docs">Documentation </a> ');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
