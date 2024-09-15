7b5e54ea65019888f6187331790a86bc9ad5f3b4
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-09-15 16:21:30 +0300

    Create register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,33 @@
+const {StatusCodes} = require('http-status-codes');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const {BadRequestError} = require('../errors');
 
+exports.register = async (req, res) => {
+    const {name, email, password} = req.body;
 
+    if (!name || !email || !password) {
+        throw new BadRequestError('Please enter the missing fields');
+    }
 
-exports.register = async(req,res)=>{
-    
-    
-    
-    res.send('Register')
+    const salt = await bcrypt.genSalt(10);
 
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-}
+    const tempUser = {email, name, password: hashedPassword};
 
+    const user = await User.create({
+        ...tempUser,
+    });
 
-exports.login = async(req,res)=>{
-    res.send('login')
-}
+    res.status(StatusCodes.CREATED).json({
+        status: 'success',
+        data: {
+            user,
+        },
+    });
+};
 
+exports.login = async (req, res) => {
+    res.send('login');
+};
